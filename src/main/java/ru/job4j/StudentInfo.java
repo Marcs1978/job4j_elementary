467bf2702868b6b5558551cee2a84f3e4ab51c7a
Viktor197825 <112583539+Viktor197825@users.noreply.github.com>
2022-09-08 15:40:48 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Viktor Grishin");
+        System.out.println("25.12.1978");
+    }
+}
