b6f6285ee6125cd35ba8ec7672df718b22aa7c04
Viktor197825 <112583539+Viktor197825@users.noreply.github.com>
2022-12-05 21:34:54 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -9,7 +9,7 @@ public class Converter {
         return value / 60;
     }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         float dollar = Converter.rubleToDollar(240);
         System.out.println("140 rubles are " + euro + " euro.");
